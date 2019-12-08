c61a4d34553db2cf8eb1d75be335956261f3d018
Lisa <alexanderlisa22@gmail.com>
2019-12-08 11:37:35 -0500

    Change titles on JSON file

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ let express = require('express');
 
 let app = express();
 
+app.get('/',function(req, res){
+  res.send('Create Submission')
+})
 
 
 
@@ -11,3 +14,4 @@ let port = 3000;
 app.listen(port, function () {
     console.log(`Express server started on port ${port}.`);
 });
+
