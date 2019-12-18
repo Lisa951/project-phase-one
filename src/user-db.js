cdad68d70a15314ac85c7c679c762746788e48c7
Lisa <alexanderlisa22@gmail.com>
2019-12-18 01:34:51 -0500

    added  postRegistrationRoute

--- src/user-db.js
+++ src/user-db.js
@@ -4,6 +4,9 @@
 let util = require('util');
 let fs = require('fs');
 let path = require('path');
+let argon2 = require('argon2');
+let db = require('../../db');
+
 
 
 
@@ -24,14 +27,7 @@ async function read() {
 
 }
 
-/**
-async function read(){
-  return readFile(dbPath).then(function (fileContents){
-    let json = JSON.parse(fileContents);
-    return allNames;
-  });
-}
-*/
+
 
 
 async function write(dbItems) {//what do we want to write in the file
@@ -52,7 +48,76 @@ async function addNames(newName) {
 }
 
 
+//route 2
+
+
+  async function postRegistrationRoute(req, res, next){
+  try {
+
+    let usernameExists = await db.usernameExists(req.body.username)
+      let formErrors = {};
+      if (!usernameExists && req.body.username) {
+        formErrrors.username = null;
+      } else {
+        formErrors.username = 'invalid username';
+      }
+
+      if (formErrors.username) {
+        res
+        .status(400)
+        .render('register', {
+          pageId: 'register',
+          title: 'Register',
+          username: req.sessions.username,
+          formErrors: formErrors,
+          formValues: {
+            username: req.body.username,
+          },
+        });
+
+      } else {
+        let hash = await argon2.hash(req.body.password);
+        await db.addUser ({
+          username :req.body.username,
+          password: hash,
+        });
+        res.redirect('/login');
+      }
+      } catch (error){
+        next(error);
+      }
+    }
+
+
+
+
+
+
+
+
+   /* let hash = await argon.hash(req.body.password);
+} catch (err) {
+  //..error
+}
+
+try{
+  if (await argon2.verify("<big long hash>", "password")) {
+    //password match
+  } else {
+    //password did not match
+  }
+} catch (err){
+  //internal failure
+}
+*/
+
+
+
+
+
 module.exports = {
   read: read,
   addcontactInfo: addNames, //the return value of reuire is the value of module dot exports its pointing at
+  get: getRegisterRoute,
+  post: postRegisterRoute,
 };
